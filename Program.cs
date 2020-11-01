8f494f97572642c5af783c54b2e405fa508cc37a
Feli <64453816+01-Feli@users.noreply.github.com>
2020-11-01 00:10:58 -0300

    1.0.0.0
    using Discord-WebHook-Sender-Library

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using WebHook_Sender_Library;
+using System;
+
+namespace WebHookSpammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this webhook spammer!");
+            Console.WriteLine("Enter a webhook to start spamming");
+            var webhook = Console.ReadLine();
+            Console.WriteLine("Okey the webhook to spam is: " + webhook);
+            Console.WriteLine("Enter the message that you want to spam!");
+            var message = Console.ReadLine();
+            Console.WriteLine("Okey the message is: " + message);
+            Console.WriteLine("Now enter the number of webhooks that you want to spam!");
+            var times = Console.ReadLine();
+            WebHookSender.Send(webhook, message, Convert.ToInt32(times));
+        }
+    }
+}
